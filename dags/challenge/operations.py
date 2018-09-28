a41c0a577c0d386694d55f8cd029f952fa79d8d8
David Olorundare <david.o@ieee.org>
2018-09-28 15:18:45 -0500

    updated unit test code

---
+++ dags/challenge/operations.py
@@ -0,0 +1,4 @@
+"""Tempus challenge  - Operations and Functions.
+
+Describes the code definition of the PythonOperator tasks in the DAG.
+"""
--- tests/unit/challenge/test_operations.py
+++ tests/unit/challenge/test_operations.py
@@ -1,4 +1,12 @@
+"""Tempus challenge  - Unit Tests.
+
+Describes unit tests for the PythonOperator tasks in the DAG.
+"""
+
 import datetime
 import pytest
 from dags import challenge as c
 
+
+
+class TestOperations:
