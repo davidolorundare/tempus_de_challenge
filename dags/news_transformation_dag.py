95e1b79cb005cd4db873cd0a0d8b8b5c43604350
David Olorundare <david.o@ieee.org>
2018-10-14 14:04:23 -0500

    updated pipeline definitions

--- dags/news_transformation_dag.py
+++ dags/news_transformation_dag.py
@@ -119,9 +119,11 @@ flatten_csv_task = PythonOperator(task_id='transform_to_csv_task',
                                   dag=dag)
 
 # upload the flattened csv into my S3 bucket
-upload_csv_task = DummyOperator(task_id='upload_csv_to_s3_task',
-                                retries=3,
-                                dag=dag)
+upload_csv_task = PythonOperator(task_id='upload_csv_to_s3_task',
+                                 provide_context=True,
+                                 python_callable=upload_func_alias,
+                                 retries=3,
+                                 dag=dag)
 
 # end workflow
 end_task = DummyOperator(task_id='end', dag=dag)
