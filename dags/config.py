a64935ec34f5f9a9c1c5476ca1f68ff5a18af24c
David Olorundare <david.o@ieee.org>
2018-10-15 12:28:11 -0500

    updated config file to support AWS CLI

--- dags/config.py
+++ dags/config.py
@@ -3,3 +3,7 @@ import os
 # api key for the news api
 os.environ["NEWS_API_KEY"] = "68ce2435405b42e5b4a90080249c6962"
 NEWS_API_KEY = "68ce2435405b42e5b4a90080249c6962"
+
+# api key for AWS CLI 'tempus_challenge_user' to use boto library
+os.environ['aws_access_key_id'] = AKIAIS5JPFXJCYF7RGVQ
+os.environ['aws_secret_access_key'] = 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
