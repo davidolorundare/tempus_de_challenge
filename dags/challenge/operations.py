1197c4c978839f9eca44a6336611c988287b57d3
David Olorundare <david.o@ieee.org>
2018-10-21 10:15:45 -0500

    updated helper_execute_json_transformation function

--- dags/challenge/operations.py
+++ dags/challenge/operations.py
@@ -1253,7 +1253,7 @@ class TransformOperations:
             for file in files:
                 key = file.split("_")[1]
                 fname = str(timestamp) + "_" + key + "_top_headlines.csv"
-                stat, msg = json_transfm_func(file, fname, reader)
+                stat = json_transfm_func(file, fname, reader)[0]
                 per_file_status.append(stat)
 
         # verify that ALL the files successfully were converted to csv
@@ -1385,12 +1385,12 @@ class TransformOperations:
 
         if len(files) == 1:
             # a single json file exists, perform direct transformation on it.
-            status, msg = json_to_csv_func(files[0], filename, reader)
+            status = json_to_csv_func(files[0], filename, reader)[0]
         else:
             # transform the json files into DataFrames and merge them into one.
             merged_dataframe = jsons_to_df_func(files, reader)
             # transform the merged DataFrame into a csv
-            status, msg = df_to_csv_func(merged_dataframe, filename)
+            status = df_to_csv_func(merged_dataframe, filename)[0]
 
         return status
 
