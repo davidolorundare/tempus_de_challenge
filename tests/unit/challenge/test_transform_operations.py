ebe6bc6cdd3761b9686bf797382d7cad4510891e
David Olorundare <david.o@ieee.org>
2018-10-21 01:08:57 -0500

    updated test_helper_execute_json_transformation_for_three_jsons_succeeds

--- tests/unit/challenge/test_transform_operations.py
+++ tests/unit/challenge/test_transform_operations.py
@@ -267,7 +267,7 @@ class TestTransformOperations:
         # indicating the single json file passed in was successfully
         # converted to a csv
         json_csv_func.side_effect = lambda files, filename, reader: True
-        jsons_df_func.side_effect = lambda files: pd.DataFrame()
+        jsons_df_func.side_effect = lambda files, reader: pd.DataFrame()
         df_csv_func.side_effect = lambda dataframe, filename: True
 
         # setup pipeline information
