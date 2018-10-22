6e481f20bd35ff17ffe17ebe409f99a4f4bc3283
David Olorundare <david.o@ieee.org>
2018-10-21 23:47:06 -0500

    updated test_upload_directory_check_unknown_error_fails

--- tests/unit/challenge/test_upload_operations.py
+++ tests/unit/challenge/test_upload_operations.py
@@ -480,12 +480,52 @@ class TestUploadOperations:
         assert stat is True
         assert val == ['stuff1.txt', 'stuff2.rtf', 'stuff3.doc']
 
-    @pytest.mark.skip
     def test_upload_directory_check_unknown_error_fails(self,
-                                                        home_directory_res):
+                                                        airflow_context):
         """returns appropiate status message on encountering errors
         reading the csv directory.
         """
+
+        # Arrange
+
+        # get the current pipeline info
+        pipeline_name = airflow_context['dag'].dag_id
+
+        # status of the directory check operation and value of the data
+        stat = None
+        msg = None
+        val = None
+
+        # path to fakes news and csv directories the function
+        # under test uses
+        csv_dir = os.path.join('tempdata', pipeline_name, 'csv')
+
+        # create dummy csv files
+        file_path_one = os.path.join(csv_dir, 'stuff1.csv')
+        file_path_two = os.path.join(csv_dir, 'stuff2.csv')
+        file_path_three = os.path.join(csv_dir, 'stuff3.csv')
+
+        with Patcher() as patcher:
+            # setup pyfakefs - the fake filesystem
+            patcher.setUp()
+
+            # create a fake filesystem directory and files to test the method
+            patcher.fs.create_dir(csv_dir)
+            patcher.fs.create_file(file_path_one, contents='1,dummy,txt')
+            patcher.fs.create_file(file_path_two, contents='2,dummy,rtf')
+            patcher.fs.create_file(file_path_three, contents='3,dummy,doc')
+
+        # Act
+            # with csv files present, success status message is returned
+            stat, msg, val = c.UploadOperations.upload_directory_check(csv_dir)
+
+            # clean up and remove the fake filesystem
+            patcher.tearDown()
+
+        # Assert
+        assert "CSV files present" in msg
+        assert stat is True
+        assert val == ['stuff1.csv', 'stuff2.csv', 'stuff3.csv']
 
     @mock_s3
     def test_upload_csv_to_s3_fails_with_empty_csv_dir(self,
